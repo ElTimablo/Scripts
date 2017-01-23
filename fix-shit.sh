9d49960d47296ed8cdcfee99273f102138f416ab
ElTimablo <t.j.k.robertson@gmail.com>
2017-01-23 04:02:38 -0500

    Delete fix-shit.sh

--- fix-shit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-nvidia-settings --assign CurrentMetaMode="DVI-D-0: nvidia-auto-select +2560+360 {ForceCompositionPipeline=On}, DP-4: nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
